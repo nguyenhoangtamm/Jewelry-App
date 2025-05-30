be002ed8377d4c1dd2796cab383e125f6e633b56
Ly Thanh Vinh <thanhvinhly369@gmail.com>
2025-05-30 22:18:36 +0700

    components shop -> jewelryapp

--- lib/screens/home/views/components/popular_products.dart
+++ lib/screens/home/views/components/popular_products.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shop/components/product/product_card.dart';
-import 'package:shop/models/product_model.dart';
-import 'package:shop/route/screen_export.dart';
+import 'package:jewelryapp/components/product/product_card.dart';
+import 'package:jewelryapp/models/product_model.dart';
+import 'package:jewelryapp/route/screen_export.dart';
 
 import '../../../../constants.dart';
 
